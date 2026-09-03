63bd68d1423d8ecc92fcaadbf920d79c20ff5256
Paul0chris <pkumwend@our.ecu.edu.au>
2026-09-03 14:02:32 +0800

    created models
    made the hunt and question models, currently as comments. no migrations yet, testing on hold

--- projectA/core/huntSessions/models.py
+++ projectA/core/huntSessions/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+'''
+class Hunt(models.Model):
+    hunt_Id = models.AutoField(primary_key=True)
+    hunt_Name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+
+
+class Question(models.Model):
+    Question_Id = models.AutoField(primary_key=True)
+    description = models.CharField(max_length=1000)
+    Question_type = models.CharField(max_length=50)
+    answer = models.CharField(max_length=250)
+'''
